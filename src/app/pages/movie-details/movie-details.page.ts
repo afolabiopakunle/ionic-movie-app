587db5c1d5c5acb92ff4e58a539ccdce6b54c082
Afolabi <opakunleafolabi@gmail.com>
2022-11-17 05:54:27 +0100

    movie poster loads

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../../services/movie.service';
+import { IMovieDetails } from './MovieDetails.interface';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  movie: IMovieDetails;
+  imageBaseURL = environment.images;
+
+  constructor(private route: ActivatedRoute,
+              private movieService: MovieService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovieDetails(id)
+      .subscribe({
+        next: (response: IMovieDetails) => {
+          this.movie = response;
+          console.log(response)
+;        }
+      });
   }
 
 }
